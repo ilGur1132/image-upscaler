a22abf44e28e8bf69b40eee9813faf9174e0fd49
ilGur1132 <ilGur1132@yandex.com>
2023-02-20 05:32:26 -0500

    Add files via upload

--- data/page/index.js
+++ data/page/index.js
@@ -1,234 +1,231 @@
-var background = {
-  "port": null,
-  "message": {},
-  "receive": function (id, callback) {
-    if (id) {
-      background.message[id] = callback;
-    }
-  },
-  "connect": function (port) {
-    chrome.runtime.onMessage.addListener(background.listener); 
-    /*  */
-    if (port) {
-      background.port = port;
-      background.port.onMessage.addListener(background.listener);
-      background.port.onDisconnect.addListener(function () {
-        background.port = null;
-      });
-    }
-  },
-  "send": function (id, data) {
-    if (id) {
-      if (background.port) {
-        if (background.port.name !== "webapp") {
-          chrome.runtime.sendMessage({
+if (window === window.top) {
+  var background = {
+    "port": null,
+    "message": {},
+    "receive": function (id, callback) {
+      if (id) {
+        background.message[id] = callback;
+      }
+    },
+    "connect": function (port) {
+      chrome.runtime.onMessage.addListener(background.listener); 
+      /*  */
+      if (port) {
+        background.port = port;
+        background.port.onMessage.addListener(background.listener);
+        background.port.onDisconnect.addListener(function () {
+          background.port = null;
+        });
+      }
+    },
+    "send": function (id, data) {
+      if (id) {
+        if (background.port) {
+          if (background.port.name !== "webapp") {
+            chrome.runtime.sendMessage({
+              "method": id,
+              "data": data,
+              "path": "interface-to-background"
+            }, function () {
+              return chrome.runtime.lastError;
+            });
+          }
+        }
+      }
+    },
+    "post": function (id, data) {
+      if (id) {
+        if (background.port) {
+          background.port.postMessage({
             "method": id,
             "data": data,
+            "port": background.port.name,
             "path": "interface-to-background"
-          }, function () {
-            return chrome.runtime.lastError;
           });
         }
       }
-    }
-  },
-  "post": function (id, data) {
-    if (id) {
-      if (background.port) {
-        background.port.postMessage({
-          "method": id,
-          "data": data,
-          "port": background.port.name,
-          "path": "interface-to-background"
-        });
-      }
-    }
-  },
-  "listener": function (e) {
-    if (e) {
-      for (let id in background.message) {
-        if (background.message[id]) {
-          if ((typeof background.message[id]) === "function") {
-            if (e.path === "background-to-interface") {
-              if (e.method === id) {
-                background.message[id](e.data);
+    },
+    "listener": function (e) {
+      if (e) {
+        for (let id in background.message) {
+          if (background.message[id]) {
+            if ((typeof background.message[id]) === "function") {
+              if (e.path === "background-to-interface") {
+                if (e.method === id) {
+                  background.message[id](e.data);
+                }
               }
             }
           }
         }
       }
     }
-  }
-};
+  };
 
-var config = {
-  "iframe": {
-    "element": document.querySelector("iframe"),
-    "window": document.querySelector("iframe").contentWindow
-  },
-  "addon": {
-    "homepage": function () {
-      return chrome.runtime.getManifest().homepage_url;
-    }
-  },
-  "resize": {
-    "timeout": null,
-    "method": function () {
-      if (config.port.name === "win") {
-        if (config.resize.timeout) window.clearTimeout(config.resize.timeout);
-        config.resize.timeout = window.setTimeout(async function () {
-          let current = await chrome.windows.getCurrent();
-          /*  */
-          config.storage.write("interface.size", {
-            "top": current.top,
-            "left": current.left,
-            "width": current.width,
-            "height": current.height
-          });
-        }, 1000);
+  var app = {
+    "iframe": {
+      "window": document.querySelector("iframe").contentWindow
+    },
+    "addon": {
+      "homepage": function () {
+        return chrome.runtime.getManifest().homepage_url;
       }
-    }
-  },
-  "port": {
-    "name": '',
-    "connect": function () {
-      config.port.name = "webapp";
-      let context = document.documentElement.getAttribute("context");
+    },
+    "prefs": {
+      set scale (val) {app.storage.write("scale", val)},
+      set level (val) {app.storage.write("level", val)},
+      set useai (val) {app.storage.write("useai", val)},
+      set usepatch (val) {app.storage.write("usepatch", val)},
+      set patchsize (val) {app.storage.write("patchsize", val)},
+      get scale () {return app.storage.read("scale") !== undefined ? app.storage.read("scale") : "4x"},
+      get useai () {return app.storage.read("useai") !== undefined ? app.storage.read("useai") : false},
+      get level () {return app.storage.read("level") !== undefined ? app.storage.read("level") : "thick"},
+      get usepatch () {return app.storage.read("usepatch") !== undefined ? app.storage.read("usepatch") : true},
+      get patchsize () {return app.storage.read("patchsize") !== undefined ? app.storage.read("patchsize") : 16}
+    },
+    "start": async function () {
+      let target = window === window.top ? app.iframe.window : window;
       /*  */
-      if (chrome.runtime) {
-        if (chrome.runtime.connect) {
-          if (context !== config.port.name) {
-            if (document.location.search === "?win") {
-              config.port.name = "win";
+      target.postMessage({
+        "from": "app",
+        "name": "storage",
+        "value": {
+          "scale": app.prefs.scale,
+          "level": app.prefs.level,
+          "useai": app.prefs.useai,
+          "usepatch": app.prefs.usepatch,
+          "patchsize": app.prefs.patchsize
+        }
+      }, '*');
+    },
+    "resize": {
+      "timeout": null,
+      "method": function () {
+        if (app.port.name === "win") {
+          if (app.resize.timeout) window.clearTimeout(app.resize.timeout);
+          app.resize.timeout = window.setTimeout(async function () {
+            let current = await chrome.windows.getCurrent();
+            /*  */
+            app.storage.write("interface.size", {
+              "top": current.top,
+              "left": current.left,
+              "width": current.width,
+              "height": current.height
+            });
+          }, 1000);
+        }
+      }
+    },
+    "port": {
+      "name": '',
+      "connect": function () {
+        app.port.name = "webapp";
+        let context = document.documentElement.getAttribute("context");
+        /*  */
+        if (chrome.runtime) {
+          if (chrome.runtime.connect) {
+            if (context !== app.port.name) {
+              if (document.location.search === "?win") {
+                app.port.name = "win";
+              }
+            }
+          }
+        }
+        /*  */
+        document.documentElement.setAttribute("context", app.port.name);
+      }
+    },
+    "load": function () {
+      const reload = document.getElementById("reload");
+      const support = document.getElementById("support");
+      const donation = document.getElementById("donation");
+      /*  */
+      reload.addEventListener("click", function () {
+        document.location.reload();
+      });
+      /*  */
+      support.addEventListener("click", function () {
+        const url = app.addon.homepage();
+        chrome.tabs.create({"url": url, "active": true});
+      }, false);
+      /*  */
+      donation.addEventListener("click", function () {
+        const url = app.addon.homepage() + "?reason=support";
+        chrome.tabs.create({"url": url, "active": true});
+      }, false);
+      /*  */
+      app.storage.load(app.start);
+      window.removeEventListener("load", app.load, false);
+    },
+    "storage": {
+      "local": {},
+      "read": function (id) {
+        return app.storage.local[id];
+      },
+      "load": function (callback) {
+        chrome.storage.local.get(null, function (e) {
+          app.storage.local = e;
+          callback();
+        });
+      },
+      "write": function (id, data) {
+        if (id) {
+          if (data !== '' && data !== null && data !== undefined) {
+            let tmp = {};
+            tmp[id] = data;
+            app.storage.local[id] = data;
+            chrome.storage.local.set(tmp);
+          } else {
+            delete app.storage.local[id];
+            chrome.storage.local.remove(id);
+          }
+        }
+      }
+    },
+    "message": function (e) {
+      if (e) {
+        if (e.data) {
+          if (e.data.from === "sandbox") {
+            if (e.data.name === "scale") {
+              app.prefs.scale = e.data.value.scale;
+            }
+            /*  */
+            if (e.data.name === "level") {
+              app.prefs.level = e.data.value.level;
+            }
+            /*  */
+            if (e.data.name === "patchsize") {
+              app.prefs.patchsize = e.data.value.patchsize;
+            }
+            /*  */
+            if (e.data.name === "usepatch") {
+              app.prefs.usepatch = e.data.value.usepatch;
+            }
+            /*  */
+            if (e.data.name === "useai") {
+              app.prefs.useai = e.data.value.useai;
+            }
+            /*  */
+            if (e.data.name === "upscaled") {
+              const a = document.createElement('a');
+              document.body.appendChild(a);
+              a.download = "upscaled." + e.data.value.ext;
+              a.href = e.data.value.src;
+              a.click();
+              //
+              window.setTimeout(function () {
+                a.remove();
+              }, 300);
             }
           }
         }
       }
-      /*  */
-      document.documentElement.setAttribute("context", config.port.name);
     }
-  },
-  "load": function () {
-    const reload = document.getElementById("reload");
-    const support = document.getElementById("support");
-    const donation = document.getElementById("donation");
-    /*  */
-    reload.addEventListener("click", function () {
-      document.location.reload();
-    });
-    /*  */
-    support.addEventListener("click", function () {
-      const url = config.addon.homepage();
-      chrome.tabs.create({"url": url, "active": true});
-    }, false);
-    /*  */
-    donation.addEventListener("click", function () {
-      const url = config.addon.homepage() + "?reason=support";
-      chrome.tabs.create({"url": url, "active": true});
-    }, false);
-    /*  */
-    config.storage.load(config.app.start);
-    window.removeEventListener("load", config.load, false);
-  },
-  "storage": {
-    "local": {},
-    "read": function (id) {
-      return config.storage.local[id];
-    },
-    "load": function (callback) {
-      chrome.storage.local.get(null, function (e) {
-        config.storage.local = e;
-        callback();
-      });
-    },
-    "write": function (id, data) {
-      if (id) {
-        if (data !== '' && data !== null && data !== undefined) {
-          let tmp = {};
-          tmp[id] = data;
-          config.storage.local[id] = data;
-          chrome.storage.local.set(tmp);
-        } else {
-          delete config.storage.local[id];
-          chrome.storage.local.remove(id);
-        }
-      }
-    }
-  },
-  "app": {
-    "start": function () {
-      config.iframe.element.src = chrome.runtime.getURL("/data/interface/index.html");
-      /*  */
-      config.iframe.element.onload = function () {
-        config.iframe.window.postMessage({
-          "from": "app",
-          "name": "storage",
-          "value": {
-            "scale": config.app.prefs.scale,
-            "level": config.app.prefs.level,
-            "useai": config.app.prefs.useai,
-            "usepatch": config.app.prefs.usepatch,
-            "patchsize": config.app.prefs.patchsize
-          }
-        }, '*');
-      }
-    },
-    "prefs": {
-      set scale (val) {config.storage.write("scale", val)},
-      set level (val) {config.storage.write("level", val)},
-      set useai (val) {config.storage.write("useai", val)},
-      set usepatch (val) {config.storage.write("usepatch", val)},
-      set patchsize (val) {config.storage.write("patchsize", val)},
-      get scale () {return config.storage.read("scale") !== undefined ? config.storage.read("scale") : "4x"},
-      get useai () {return config.storage.read("useai") !== undefined ? config.storage.read("useai") : false},
-      get level () {return config.storage.read("level") !== undefined ? config.storage.read("level") : "thick"},
-      get usepatch () {return config.storage.read("usepatch") !== undefined ? config.storage.read("usepatch") : true},
-      get patchsize () {return config.storage.read("patchsize") !== undefined ? config.storage.read("patchsize") : 16}
-    }
-  },
-  "message": function (e) {
-    if (e) {
-      if (e.data) {
-        if (e.data.from === "sandbox") {
-          if (e.data.name === "scale") {
-            config.app.prefs.scale = e.data.value.scale;
-          }
-          /*  */
-          if (e.data.name === "level") {
-            config.app.prefs.level = e.data.value.level;
-          }
-          /*  */
-          if (e.data.name === "patchsize") {
-            config.app.prefs.patchsize = e.data.value.patchsize;
-          }
-          /*  */
-          if (e.data.name === "usepatch") {
-            config.app.prefs.usepatch = e.data.value.usepatch;
-          }
-          /*  */
-          if (e.data.name === "useai") {
-            config.app.prefs.useai = e.data.value.useai;
-          }
-          /*  */
-          if (e.data.name === "upscaled") {
-            const a = document.createElement('a');
-            document.body.appendChild(a);
-            a.download = "upscaled." + e.data.value.ext;
-            a.href = e.data.value.src;
-            a.click();
-            //
-            window.setTimeout(function () {
-              a.remove();
-            }, 300);
-          }
-        }
-      }
-    }
-  }
-};
-
-config.port.connect();
-
-window.addEventListener("load", config.load, false);
-window.addEventListener("message", config.message, false);
-window.addEventListener("resize", config.resize.method, false);
+  };
+  //
+  app.port.connect();
+  //
+  window.addEventListener("load", app.load, false);
+  window.addEventListener("message", app.message, false);
+  window.addEventListener("resize", app.resize.method, false);
+}
